f0f59d12dbb86a1612e5ee1a2db5d5af8c0cb8ed
bhanupratap1810 <bhanupratapbd1@gmail.com>
2020-12-31 11:47:03 +0530

    middlewares update

--- server.go
+++ server.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 	"golang-rest-api/controller"
+	"golang-rest-api/middlewares"
 	"golang-rest-api/service"
+	"io"
+	"os"
 )
 
 var (
@@ -11,8 +15,18 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ :=os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
